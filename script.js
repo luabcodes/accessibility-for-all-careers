c27b976b71019c5c36e8ed000cc8c45c398bfc4e
Luana Bentes <luabentes94@gmail.com>
2025-12-05 22:46:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function increaseFont() {
+    document.body.style.fontSize = "larger";
+}
+
+function decreaseFont() {
+    document.body.style.fontSize = "smaller";
+}
+
+function toggleContrast() {
+    document.body.classList.toggle("high-contrast");
+}
+
+function toggleSignLanguage() {
+    alert("This space will support ASL videos soon!");
+}
+
+function toggleScreenReader() {
+    alert("Screen reader mode activated.");
+}
